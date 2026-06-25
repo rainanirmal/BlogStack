4fb0e9d814f843ded95c33d8551028afeaefd3bf
rainanirmal <rainanirmal01@gmail.com>
2026-06-25 23:52:18 +0530

    feat : add user schema and model using Mongoose

---
+++ model/user.js
@@ -0,0 +1,34 @@
+const { Schema , model} = require("mongoose");
+
+const userSchema = new Schema({
+    fullName : {
+        type : String,
+        required : true,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    salt : {
+        type : String,
+        required : true,
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    profileImaggeURL : {
+        type : String,
+        default : '/images/user.png',
+    },
+    role : {
+        type : String,
+        enum : ["USER" , "ADMIN"],
+        default : "USER",
+    },
+} , { timestamps : true});
+
+const User = model("user" , userSchema);
+
+module.exports = User;
