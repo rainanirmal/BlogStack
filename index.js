9c17fcd2872d98970c1d288bdfab87fea311da12
rainanirmal <rainanirmal01@gmail.com>
2026-06-25 22:19:19 +0530

    feat : setup head.ejs and script.ejs partials with Bootstrap

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 app.set("view engine" , "ejs");
 app.set("views" , path.resolve("./views"));
 
+app.get('/' , (request , response) => {
+    response.render("home");
+})
+
 app.listen(PORT , () => {
     console.log(`Server started on port ${PORT}`);
 });
