ae0e5c5464031bb055d4ab49f685b0e9a5d1eb9e
rainanirmal <rainanirmal01@gmail.com>
2026-06-26 19:26:32 +0530

    feat : add pre-save middleware for password hashing

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const { Schema , model} = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
 
 const userSchema = new Schema({
     fullName : {
@@ -28,6 +29,23 @@ const userSchema = new Schema({
         default : "USER",
     },
 } , { timestamps : true});
+
+userSchema.pre('save' , function (next) {
+
+    const user = this;
+
+    if(!user.isModified("password")) {
+        return;
+    }
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac('sha256' , salt).update(user.password).digest('hex');
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+})
 
 const User = model("user" , userSchema);
 
