0e4c60dd6d607463c3e714ed94dfec05a6f7bab0
rainanirmal <rainanirmal01@gmail.com>
2026-06-28 21:19:21 +0530

    feat : connect application to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 const app = express();
 
-app.set("view engine" , "ejs");
-app.set("views" , path.resolve("./views"));
+mongoose.connect("mongodb://localhost:27017/blogStack").then(() => {console.log("MongoDB connected !")});
 
-app.get('/' , (request , response) => {
-    response.render("home");
-})
+app.use(express.urlencoded({ extended : false}));
 
-app.listen(PORT , () => {
-    console.log(`Server started on port ${PORT}`);
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
