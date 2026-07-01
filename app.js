8d080209ab70876276e2e8689332f9ad7a7fa607
rainanirmal <rainanirmal01@gmail.com>
2026-07-01 21:40:46 +0530

    refactor : use environment variable for server port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { checkForAuthenticatonCookie } = require("./middlewares/authentication");
 
 const Blog = require("./model/blog");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL).then(() => {console.log("MongoDB connected !")});
